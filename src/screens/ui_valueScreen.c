f3a0dda524a8d807230f9c6ba25e485448597044
Beto Dealmeida <roberto@dealmeida.net>
2026-01-26 16:41:11 -0500

    Fix font

--- src/screens/ui_valueScreen.c
+++ src/screens/ui_valueScreen.c
@@ -48,7 +48,7 @@ lv_obj_set_align( ui_posindSha, LV_ALIGN_CENTER );
 lv_label_set_text(ui_posindSha,"123");
 lv_obj_set_style_text_color(ui_posindSha, lv_color_hex(0xAFAFAF), LV_PART_MAIN | LV_STATE_DEFAULT );
 lv_obj_set_style_text_opa(ui_posindSha, 255, LV_PART_MAIN| LV_STATE_DEFAULT);
-lv_obj_set_style_text_font(ui_posindSha, &ui_font_SGdig60, LV_PART_MAIN| LV_STATE_DEFAULT);
+lv_obj_set_style_text_font(ui_posindSha, &ui_font_SG12h60, LV_PART_MAIN| LV_STATE_DEFAULT);
 
 ui_profileName = lv_label_create(ui_dataScreen);
 lv_obj_set_width( ui_profileName, lv_pct(78));
